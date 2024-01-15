0df85e7d0e70d5fd017bdb76c5f4da647362b686
HL <kibisama9@gmail.com>
2024-01-15 17:17:43 +0900

    1/15/24

--- middlewares/puppeteer/smartSourcePuppetFn.js
+++ middlewares/puppeteer/smartSourcePuppetFn.js
@@ -17,47 +17,56 @@ const smartSourcePuppetFn = function ({ waitForOptions, xPaths }) {
         smartSourceCost =
           (await page.findTexts(xPaths.catalogSearch.acqCost))[0] ?? '';
         await page.setRandomDelay(0, `copied SmartSource Cost[NDC: ${ndc}]`);
-        if (!smartSourceCost) {
-          if (altNDC.length > 0) {
-            for (let i = 0; i < altNDC.length; i++) {
-              const searchBar = await page.$x(xPaths.menu.searchBar);
-              if (searchBar.length > 0) {
-                await page.typeInputEl(altNDC[i], searchBar[0]);
-                await page.waitForElements(
-                  `//div[@class= "results-wrapper"] //span[@class= "term" and contains(text(), "${altNDC[i]}")]`,
-                  xPaths.catalogSearch.ndcSearchResult,
-                );
-                smartSourceAltCost[i] =
-                  (await page.findTexts(xPaths.catalogSearch.acqCost))[0] ?? '';
-                if (!smartSourceAltCost[i]) {
-                  continue;
-                }
-                const els = await this.findAltItems(page, ndc);
-                for (let j = i + 1; j < altNDC.length; j++) {
-                  for (let k = 0; k < els[1].length; k++) {
-                    if (altNDC[j] === els[1][k]) {
-                      smartSourceAltName[j] = els[0][k];
-                      smartSourceAltCost[j] = els[2][k];
-                      break;
-                    }
-                  }
-                }
+        if (!smartSourceName) {
+          for (let i = 0; i < altNDC.length; i++) {
+            const searchBar = await page.$x(xPaths.menu.searchBar);
+            if (searchBar.length > 0) {
+              await page.typeInputEl(altNDC[i], searchBar[0]);
+              await page.waitForElements(
+                `//div[@class= "results-wrapper"] //span[@class= "term" and contains(text(), "${altNDC[i]}")]`,
+                xPaths.catalogSearch.ndcSearchResult,
+              );
+              smartSourceAltCost[i] =
+                (await page.findTexts(xPaths.catalogSearch.acqCost))[0] ?? '';
+              if (!smartSourceAltCost[i]) {
+                continue;
+              }
+              const autocompleteBox = await page.$x(
+                xPaths.catalogSearch.autocompleteBox,
+              );
+              if (autocompleteBox.length > 0) {
+                await page.clickEl(xPaths.menu.searchInputClear);
+              }
+              const els = await this.findAltItems(page, ndc);
+              if (els.length === 0) {
                 break;
               }
+              for (let j = i + 1; j < altNDC.length; j++) {
+                for (let k = 0; k < els[1].length; k++) {
+                  if (altNDC[j] === els[1][k]) {
+                    smartSourceAltName[j] = els[0][k];
+                    smartSourceAltCost[j] = els[2][k];
+                    break;
+                  }
+                }
+              }
+              break;
             }
           }
         } else {
           const els = await this.findAltItems(page, ndc);
-          altNDC.forEach((v, i) => {
-            const j = els[1].indexOf(v);
-            if (j > -1) {
-              smartSourceAltName[i] = els[0][j];
-              smartSourceAltCost[i] = els[2][j];
-            } else {
-              smartSourceAltName[i] = '';
-              smartSourceAltCost[i] = '';
-            }
-          });
+          if (els.length > 0) {
+            altNDC.forEach((v, i) => {
+              const j = els[1].indexOf(v);
+              if (j > -1) {
+                smartSourceAltName[i] = els[0][j];
+                smartSourceAltCost[i] = els[2][j];
+              } else {
+                smartSourceAltName[i] = '';
+                smartSourceAltCost[i] = '';
+              }
+            });
+          }
         }
       }
       const dateLastUpdatedSmartSource = new Date(Date.now());
@@ -81,12 +90,19 @@ const smartSourcePuppetFn = function ({ waitForOptions, xPaths }) {
     },
     async findAltItems(page, ndc) {
       await page.clickEl(xPaths.catalogSearch.findAltButton);
-      const els = await page.waitForElements([
+      let els = await page.waitForElements([
         xPaths.findAlternatives.altName,
         xPaths.findAlternatives.altNDC,
         xPaths.findAlternatives.altACQCost,
       ]);
-      els[1] = els[1].map((name) => name.substring(7));
+      if (els.length > 0) {
+        els[1] = els[1].map((ndc) => ndc.substring(7));
+      } else {
+        const noAlt = await page.$x(xPaths.findAlternatives.noAlt);
+        if (noAlt.length > 0) {
+          els = [];
+        }
+      }
       await page.clickEl(xPaths.findAlternatives.closeModalButton);
       await page.setRandomDelay(0, `found SmartSource AltItems[NDC: ${ndc}]`);
       return els;
--- middlewares/puppeteer/smartSourceXPaths.js
+++ middlewares/puppeteer/smartSourceXPaths.js
@@ -2,6 +2,7 @@ const xPaths = {
   menu: {
     homeLink: '//a[@title= "SmartSource Logo"]',
     searchBar: '//input[@id= "js-site-search-input"]',
+    searchInputClear: '//span[@class= "product-search-input-clear"]',
   },
   catalogSearch: {
     // noProductsFound: '//div[contains(text(), "No products found"]',
@@ -11,6 +12,7 @@ const xPaths = {
     ndcSearchResult:
       '//div[contains(text(), "No products found")] | //div[@class= "indicator-acq-cost"]',
     findAltButton: '//button[contains(text(), "Find Alternatives")]',
+    autocompleteBox: '//ul[@tabindex= "0"]',
   },
   findAlternatives: {
     altName:
@@ -20,6 +22,9 @@ const xPaths = {
     altACQCost:
       '//div[contains(text(), "All Alternatives (")] /.. /.. //div[position()= 3] //div[@class= "indicator-acq-cost"]',
     closeModalButton: '//div[@title= "Close Find Alternatives dialog"]',
+    header: '//h2[contains(text(), "Find Alternatives")]',
+    noAlt:
+      '//div[contains(text(), "There are no alternatives for this product")]',
   },
 };
 
